e934f42f973bfe2ec8d2197e2536da1ac80efb34
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-03 05:45:03 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Yuri G All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function displays an alert.
+ */
+function myButtonClicked() {
+  const TAX = 1.13
+  const cone = 2
+  const size = parseFloat(document.getElementById("ice-cream-size").value)
+  const topping = parseFloat(document.getElementById("ice-cream-toppings").value)
+
+  const basePriceNoTAX = size + topping
+  const basePrice = basePriceNoTAX * TAX
+  const onCone = (basePriceNoTAX + cone) * TAX
+
+  const buttonOnChecked = document.getElementById("flash1").checked
+
+  if (buttonOnChecked == true) {
+    document.getElementById("Cost").innerHTML =
+      "Your total is: $" + onCone.toFixed(2) + " including tax."
+  } else {
+    document.getElementById("Cost").innerHTML =
+      "Your total is: $" + basePrice.toFixed(2) + " including tax."
+  }
+}
